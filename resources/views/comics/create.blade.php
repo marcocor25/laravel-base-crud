1cde83182220eb2952b1df4d5b260a0e1391166f
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-04-27 17:03:50 +0200

    .

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -12,23 +12,30 @@
         <label for="title">Titolo fumetto</label>
         <input type="text" name="title" id="title" placeholder="Es. Batman Beyond..." required>
 
-        <label for="series">Collezione</label>
+        <label for="thumb">Link copertina</label>
+        <input type="text" name="thumb" id="thumb" placeholder="Es. https://www.dccomics.com/..." required>
+
+        <label for="series">Serie / Collezione</label>
         <input type="text" name="series" id="series" placeholder="Es. American Vampire 1976..." required>
 
-        <label for="price">Prezzo</label>
-        <input type="text" name="price" id="price" placeholder="Inserisci un prezzo..." required>
-
-        <label for="description">Descrizione</label>
-        <textarea name="description" id="description" cols="30" rows="10" placeholder="Inserisci una descrizione..." required>
-        </textarea>
-
-        <label for="type">Tipo fumetto</label>
+        <label for="type">Tipologia fumetto</label>
         <select name="type" id="type" required>
             <option value="" selected disabled>Seleziona una tipologia</option>
             <option value="comic_book">Comic book</option>
             <option value="graphic_novel">Graphic novel</option>
         </select>
 
+        <label for="sale_date">Data pubblicazione</label>
+        <input type="date" name="sale_date" id="sale_date" required>
+
+        <label for="price">Prezzo al pubblico</label>
+        <input type="text" name="price" id="price" placeholder="Inserisci un prezzo..." required>
+
+        <label for="description">Descrizione</label>
+        <textarea name="description" id="description" cols="30" rows="10" placeholder="Inserisci una descrizione..." required>
+        </textarea>
+
         <input type="submit" value="Invia" id="submint">
+
     </form>
 @endsection
