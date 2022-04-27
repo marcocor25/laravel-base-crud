78af781a336e913676f6b804ded2682be2252054
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-04-27 16:51:04 +0200

    .

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,22 +5,25 @@
 @section('mainContent')
     <h1>Inserimento nuovo articolo</h1>
 
-    <form action="">
+    <form action="{{ route('comics.store') }}" method="POST">
+
+        @csrf
 
         <label for="title">Titolo fumetto</label>
-        <input type="text" name="title" id="title" placeholder="Es. Batman Beyond...">
+        <input type="text" name="title" id="title" placeholder="Es. Batman Beyond..." required>
 
         <label for="series">Collezione</label>
-        <input type="text" name="series" id="series" placeholder="Es. American Vampire 1976...">
+        <input type="text" name="series" id="series" placeholder="Es. American Vampire 1976..." required>
 
         <label for="price">Prezzo</label>
-        <input type="text" name="price" id="price" placeholder="Inserisci un prezzo...">
+        <input type="text" name="price" id="price" placeholder="Inserisci un prezzo..." required>
 
         <label for="description">Descrizione</label>
-        <textarea name="description" id="description" cols="30" rows="10" placeholder="Inserisci una descrizione..."></textarea>
+        <textarea name="description" id="description" cols="30" rows="10" placeholder="Inserisci una descrizione..." required>
+        </textarea>
 
         <label for="type">Tipo fumetto</label>
-        <select name="type" id="type">
+        <select name="type" id="type" required>
             <option value="" selected disabled>Seleziona una tipologia</option>
             <option value="comic_book">Comic book</option>
             <option value="graphic_novel">Graphic novel</option>
