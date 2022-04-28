b3cb4d01e2e32b4d82f1f3bbf010386b292384cd
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-04-28 16:56:20 +0200

    Piccole correzioni

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -10,26 +10,26 @@
         @csrf
 
         <label for="title">Titolo fumetto</label>
-        <input type="text" name="title" id="title" placeholder="Es. Batman Beyond..." required>
+        <input type="text" name="title" id="title" placeholder="Es. Batman Beyond...">
 
         <label for="thumb">Link copertina</label>
-        <input type="text" name="thumb" id="thumb" placeholder="Es. https://www.dccomics.com/..." required>
+        <input type="text" name="thumb" id="thumb" placeholder="Es. https://www.dccomics.com/...">
 
         <label for="series">Serie / Collezione</label>
-        <input type="text" name="series" id="series" placeholder="Es. American Vampire 1976..." required>
+        <input type="text" name="series" id="series" placeholder="Es. American Vampire 1976...">
 
         <label for="type">Tipologia fumetto</label>
-        <select name="type" id="type" required>
+        <select name="type" id="type">
             <option value="" selected disabled>Seleziona una tipologia</option>
             <option value="comic book">Comic book</option>
             <option value="graphic novel">Graphic novel</option>
         </select>
 
         <label for="sale_date">Data pubblicazione</label>
-        <input type="date" name="sale_date" id="sale_date" required>
+        <input type="date" name="sale_date" id="sale_date">
 
         <label for="price">Prezzo al pubblico</label>
-        <input type="text" name="price" id="price" placeholder="Inserisci un prezzo..." required>
+        <input type="text" name="price" id="price" placeholder="Inserisci un prezzo...">
 
         <label for="description">Descrizione</label>
         <textarea name="description" id="description" cols="30" rows="10" placeholder="Inserisci una descrizione..."></textarea>
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -12,26 +12,26 @@
         @method('PUT')
 
         <label for="title">Titolo fumetto</label>
-        <input value="{{ $comic->title }}" type="text" name="title" id="title" placeholder="Es. Batman Beyond..." required>
+        <input value="{{ $comic->title }}" type="text" name="title" id="title" placeholder="Es. Batman Beyond...">
 
         <label for="thumb">Link copertina</label>
-        <input value="{{ $comic->thumb }}" type="text" name="thumb" id="thumb" placeholder="Es. https://www.dccomics.com/..." required>
+        <input value="{{ $comic->thumb }}" type="text" name="thumb" id="thumb" placeholder="Es. https://www.dccomics.com/...">
 
         <label for="series">Serie / Collezione</label>
-        <input value="{{ $comic->series }}" type="text" name="series" id="series" placeholder="Es. American Vampire 1976..." required>
+        <input value="{{ $comic->series }}" type="text" name="series" id="series" placeholder="Es. American Vampire 1976...">
 
         <label for="type">Tipologia fumetto</label>
-        <select name="type" id="type" required>
+        <select name="type" id="type">
             <option selected disabled>Seleziona una tipologia</option>
             <option value="comic book" {{ $comic->type == 'comic book' ? 'selected' : '' }}>Comic book</option>
             <option value="graphic novel" {{ $comic->type == 'graphic novel' ? 'selected' : '' }}>Graphic novel</option>
         </select>
 
         <label for="sale_date">Data pubblicazione</label>
-        <input value="{{ $comic->sale_date }}" type="date" name="sale_date" id="sale_date" required>
+        <input value="{{ $comic->sale_date }}" type="date" name="sale_date" id="sale_date">
 
         <label for="price">Prezzo al pubblico</label>
-        <input value="{{ $comic->price }}" type="text" name="price" id="price" placeholder="Inserisci un prezzo..." required>
+        <input value="{{ $comic->price }}" type="text" name="price" id="price" placeholder="Inserisci un prezzo...">
 
         <label for="description">Descrizione</label>
         <textarea name="description" id="description" cols="30" rows="10" placeholder="Inserisci una descrizione...">{{ $comic->description }}</textarea>
